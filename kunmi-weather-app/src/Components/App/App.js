98b1d5a46b4e739af1e4039be269397043e0192d
Kunmi Williams <kumswilliams@gmail.com>
2021-01-27 12:23:46 +0000

    chnaged API KEY

--- kunmi-weather-app/src/Components/App/App.js
+++ kunmi-weather-app/src/Components/App/App.js
@@ -2,15 +2,15 @@ import { useState, useEffect } from "react";
 import styles from "./App.module.css";
 import DisplayForecast from "../DisplayForecast";
 import Input from "../Input";
-// process.env.REACT_APP_API_KEY
+//
 const api = {
-  key: "005dd360cef1e74a202e60ea8db6ff17",
+  key: process.env.REACT_APP_API_KEY,
   base: "http://api.openweathermap.org/data/2.5/",
 };
 
 function App() {
   const [forecastData, setForecastData] = useState(null);
-  const [location, setLocation] = useState("Birmingham");
+  const [location, setLocation] = useState("");
 
   useEffect(() => {
     async function getForecast() {
@@ -31,20 +31,22 @@ function App() {
   return (
     <div className={styles.App}>
       <main className={styles.container}>
-        <h2 className={styles.header}>Search by Location</h2>
+        <h2 className={styles.header}>Enter a City</h2>
         <Input setLocation={setLocation} />
-        <section className={styles.info}>
-          {forecastData?.cod === 200 ? (
-            <div>
-              <DisplayForecast
-                location={location}
-                forecastData={forecastData}
-              />
-            </div>
-          ) : (
-            <h4> {forecastData?.message} </h4>
-          )}
-        </section>
+        {location && (
+          <section className={styles.info}>
+            {forecastData?.cod === 200 ? (
+              <div>
+                <DisplayForecast
+                  location={location}
+                  forecastData={forecastData}
+                />
+              </div>
+            ) : (
+              <h4> {forecastData?.message} </h4>
+            )}
+          </section>
+        )}
       </main>
     </div>
   );
