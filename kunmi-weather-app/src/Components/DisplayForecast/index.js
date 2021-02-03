04e6400814608bf5bf19f04708f7ebd40aa1450c
Kunmi Williams <kumswilliams@gmail.com>
2021-02-03 15:00:41 +0000

    added a use effect

--- kunmi-weather-app/src/Components/DisplayForecast/index.js
+++ kunmi-weather-app/src/Components/DisplayForecast/index.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./DisplayForecast.module.css";
-// import { motion, AnimatePresence } from "framer-motion";
 
 function DisplayForecast({ forecastData, location }) {
   const [celsiusTemp, setCelsiusTemp] = useState(forecastData.main.temp);
   const [isClicked, setIsClicked] = useState(false);
   // toggle from farenheit to degrees
+
+  useEffect(() => {
+    handleClick();
+  }, [location]);
 
   function handleClick(farenheit) {
     setIsClicked(!isClicked);
@@ -18,22 +21,17 @@ function DisplayForecast({ forecastData, location }) {
 
   return (
     <div className={styles.forecastData}>
-      {/* <motion.div
-        className={styles.info}
-        animate={{ scale: 1 }}
-        transition={{ duration: 0.5, opacity: 1 }}
-      > */}
       <h2> Today's weather</h2>
       <h3>Forecast for {location} </h3>
       <h3>
         {isClicked
           ? celsiusTemp + " celsius"
-          : Math.round(forecastData?.main.temp) + "  farenheit"}
+          : Math.round(forecastData.main.temp) + "  farenheit"}
       </h3>
       <label className={styles.switch}>
         <input
           type="checkbox"
-          onClick={() => handleClick(forecastData?.main.temp)}
+          onClick={() => handleClick(forecastData.main.temp)}
         />
         <span className={styles.sliderRound}></span>
       </label>
@@ -42,7 +40,6 @@ function DisplayForecast({ forecastData, location }) {
         src={`http://openweathermap.org/img/wn/${forecastData.weather[0].icon}@4x.png`}
         alt={`icon showing a description of ${location}'s weather - ${forecastData.weather[0].description}`}
       />
-      {/* </motion.div> */}
     </div>
   );
 }
